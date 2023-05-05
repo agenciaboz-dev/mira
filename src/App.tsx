8b993eaac377d08da65150ede3009b7aaf719ddc
nandobfer <nandobfer@yahoo.com.br>
2023-05-05 09:36:10 -0300

    feat: added mui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
+import { ThemeProvider } from "@mui/material"
 import React from "react"
 import { Route, Routes } from "react-router"
 import { BrowserRouter } from "react-router-dom"
-import { Scanner } from "./components/Scanner"
+import { useMuiTheme } from "./hooks/useMuiTheme"
 import { Camera } from "./pages/Camera"
 import "./sass/_all.scss"
 
 function App() {
+    const muiTheme = useMuiTheme()
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Camera />} />
-            </Routes>
-        </BrowserRouter>
+        <ThemeProvider theme={muiTheme}>
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Camera />} />
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
     )
 }
 
---
+++ src/hooks/useMuiTheme.ts
@@ -0,0 +1,38 @@
+import { createTheme } from "@mui/material"
+import { useColors } from "./useColors"
+
+export const useMuiTheme = () => {
+    const colors = useColors()
+    const THEME = createTheme({
+        typography: {
+            //  "fontFamily": ["Poppins"].join(','),
+            //  "fontSize": 14,
+            //  "fontWeightLight": 300,
+            //  "fontWeightRegular": 400,
+            //  "fontWeightMedium": 500
+        },
+        palette: {
+            // mode: 'dark',
+
+            primary: {
+                main: colors.purple,
+            },
+            secondary: {
+                main: "#fff",
+            },
+            text: {
+                primary: colors.purple,
+                // secondary: colors.primary,
+                // disabled: colors.primary,
+            },
+            success: {
+                main: colors.green,
+            },
+            // error: {
+            //     main: colors.red,
+            // }
+        },
+    })
+
+    return THEME
+}
