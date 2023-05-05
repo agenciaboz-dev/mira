bd631191fb278a357eef1fe7a0b50ba656e8f345
nandobfer <nandobfer@yahoo.com.br>
2023-05-05 11:41:33 -0300

    refac: code logic

--- src/components/ProductModal/index.tsx
+++ src/components/ProductModal/index.tsx
@@ -18,7 +18,7 @@ interface ProductModalProps {
 }
 
 export const ProductModal: React.FC<ProductModalProps> = ({ open, setOpen, result }) => {
-    const [error, setError] = useState("")
+    const [error, setError] = useState(false)
     const [loading, setLoading] = useState(true)
     const [product, setProduct] = useState<Product>()
 
@@ -41,11 +41,17 @@ export const ProductModal: React.FC<ProductModalProps> = ({ open, setOpen, resul
 
     useEffect(() => {
         if (open) {
-            if (!Number(result)) {
-                setError("Não foi possível reconhecer esse código QR")
+            const splitted = result.split("/")
+            if (splitted.length == 2) {
+                if (splitted[0] == "mirasuprimentos") {
+                    const id = Number(splitted[1])
+                    setError(false)
+                    setProduct(products.filter((item) => item.id == id)[0])
+                } else {
+                    setError(true)
+                }
             } else {
-                setError("")
-                setProduct(products.filter((item) => item.id == Number(result))[0])
+                setError(true)
             }
         }
     }, [open])
@@ -63,7 +69,9 @@ export const ProductModal: React.FC<ProductModalProps> = ({ open, setOpen, resul
                     {!!error ? (
                         <>
                             <DialogTitle sx={title_style}>Erro</DialogTitle>
-                            <DialogContent sx={content_container_style}>{error}</DialogContent>
+                            <DialogContent sx={content_container_style}>
+                                Não foi possível reconhecer esse código QR
+                            </DialogContent>
                         </>
                     ) : (
                         <>
