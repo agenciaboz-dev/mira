f524c8fe3d799079a569c68d65c0f866c0e23b86
nandobfer <nandobfer@yahoo.com.br>
2023-05-05 19:02:34 -0300

    feat: added input

---
+++ src/components/TextField/index.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import { TextField as Text } from "@mui/material"
+import { TextFieldProps } from "@mui/material"
+import styled from "@emotion/styled"
+
+const StyledTextField = styled(Text)`
+    & .MuiOutlinedInput-root {
+        &.Mui-focused fieldset {
+            border-color: transparent;
+        }
+    }
+
+    & .MuiOutlinedInput-input {
+        background-color: white;
+        border-radius: 10vw;
+        &::placeholder {
+            color: #555555;
+            opacity: 1;
+            font-weight: bold;
+        }
+    }
+`
+
+export const TextField: React.FC<TextFieldProps> = (props) => {
+    return (
+        <StyledTextField
+            {...props}
+            InputProps={{
+                sx: {
+                    backgroundColor: "white",
+                    borderRadius: "10vw",
+                },
+            }}
+        />
+    )
+}
--- src/pages/Login/LoginForm.tsx
+++ src/pages/Login/LoginForm.tsx
@@ -1,14 +1,51 @@
+import { Button } from "@mui/material"
+import { Formik, Form } from "formik"
 import React from "react"
+import { TextField } from "../../components/TextField"
 
 interface LoginFormProps {
     onSwitch: () => void
 }
 
+interface formValues {
+    login: string
+    password: string
+}
+
 export const LoginForm: React.FC<LoginFormProps> = ({ onSwitch }) => {
+    const initialValues: formValues = { login: "", password: "" }
+    const handleSubmit = (values: formValues) => {
+        console.log(values)
+    }
+
     return (
         <div className="LoginForm-Component">
             <h1>Login</h1>
             <p>Por favor, faça o login para continuar</p>
+
+            <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+                {({ values, handleChange }) => (
+                    <Form>
+                        <TextField
+                            name="login"
+                            placeholder="E-mail"
+                            value={values.login}
+                            onChange={handleChange}
+                            fullWidth
+                        />
+                        <TextField
+                            name="password"
+                            placeholder="Senha"
+                            value={values.password}
+                            onChange={handleChange}
+                            fullWidth
+                        />
+                        <Button type="submit" variant="contained">
+                            Entrar
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
 
             <div className="signup-text">
                 <p>Não tem conta?</p>
