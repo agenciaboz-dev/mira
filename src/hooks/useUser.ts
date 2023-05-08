bc25b550624c11a4a27dbc70541e9b3c1759f4db
nandobfer <nandobfer@yahoo.com.br>
2023-05-08 16:01:04 -0300

    feat: added logout feature

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,8 +1,15 @@
 import { useContext } from "react"
+import { useNavigate } from "react-router-dom"
 import UserContext from "../contexts/userContext"
 
 export const useUser = () => {
     const userContext = useContext(UserContext)
+    const navigate = useNavigate()
 
-    return { user: userContext.value, setUser: userContext.setValue }
+    const logout = () => {
+        userContext.setValue(null)
+        navigate("/")
+    }
+
+    return { user: userContext.value, setUser: userContext.setValue, logout }
 }
