770cb5a566dbd49e0328e2c6193938fc4e09289f
nandobfer <nandobfer@yahoo.com.br>
2023-05-08 09:26:59 -0300

    feat: added api

---
+++ src/hooks/useApi.ts
@@ -0,0 +1,30 @@
+import { api } from "../api"
+
+interface ApiOptions {
+    data?: any
+    callback: Function
+    errorCallback?: Function
+    finallyCallback?: Function
+}
+
+export const useApi = () => {
+    const defaultError = (error: Error, errorCallback?: Function) => {
+        errorCallback && errorCallback()
+        console.error(error)
+    }
+
+    const defaultFinally = (finallyCallback?: Function) => {
+        finallyCallback && finallyCallback()
+    }
+
+    const methods = {
+        login: (options: ApiOptions) => {
+            api.post("/login", options.data)
+                .then((response) => options.callback(response))
+                .catch((error) => defaultError(error, options.errorCallback))
+                .finally(() => defaultFinally(options.finallyCallback))
+        },
+    }
+
+    return methods
+}
