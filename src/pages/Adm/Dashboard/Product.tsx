8c5e617449057da8b1779779ba06532790dcd400
nandobfer <nandobfer@yahoo.com.br>
2023-05-11 17:17:27 -0300

    update: info-container

--- src/pages/Adm/Dashboard/Product.tsx
+++ src/pages/Adm/Dashboard/Product.tsx
@@ -2,18 +2,23 @@ import React from "react"
 import { Product as ProductType } from "../../../definitions/product"
 import EditIcon from "@mui/icons-material/Edit"
 import { IconButton, Paper } from "@mui/material"
+import { CurrencyText } from "../../../components/CurrencyText"
 
 interface ProductProps {
     product: ProductType
-    setProduct: (product:ProductType) => void
+    setProduct: (product: ProductType) => void
 }
 
 export const Product: React.FC<ProductProps> = ({ product, setProduct }) => {
     return (
         <Paper className="Product-Component">
-            <h3>{product.name}</h3>
+            <div className="info-container">
+                <h3>{product.name}</h3>
+                <CurrencyText value={product.price} />
+                <h3>{product.stock}</h3>
+            </div>
             <IconButton onClick={() => setProduct(product)}>
-                <EditIcon color='primary' />
+                <EditIcon color="primary" />
             </IconButton>
         </Paper>
     )
