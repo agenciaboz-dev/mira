06e6ba713ab5fa586a94c67b5c2fa941ccc80294
nandobfer <nandobfer@yahoo.com.br>
2023-05-11 18:01:47 -0300

    update: number format to stock

--- src/pages/Adm/Dashboard/Product.tsx
+++ src/pages/Adm/Dashboard/Product.tsx
@@ -3,6 +3,8 @@ import { Product as ProductType } from "../../../definitions/product"
 import EditIcon from "@mui/icons-material/Edit"
 import { IconButton, Paper } from "@mui/material"
 import { CurrencyText } from "../../../components/CurrencyText"
+import QrCodeScannerIcon from "@mui/icons-material/QrCodeScanner"
+import CurrencyFormat from "react-currency-format"
 
 interface ProductProps {
     product: ProductType
@@ -13,13 +15,27 @@ export const Product: React.FC<ProductProps> = ({ product, setProduct }) => {
     return (
         <Paper className="Product-Component">
             <div className="info-container">
-                <h3>{product.name}</h3>
-                <CurrencyText value={product.price} />
-                <h3>{product.stock}</h3>
+                <h3 style={{ flex: "0.4" }}>{product.name}</h3>
+                <CurrencyText style={{ flex: "0.4" }} value={product.price} />
+                <CurrencyFormat
+                    value={product.stock}
+                    displayType="text"
+                    thousandSeparator="."
+                    decimalSeparator=","
+                    decimalScale={2}
+                    fixedDecimalScale={false}
+                    prefix={""}
+                    style={{ flex: "0.4" }}
+                />
             </div>
-            <IconButton onClick={() => setProduct(product)}>
-                <EditIcon color="primary" />
-            </IconButton>
+            <div className="actions-container">
+                <IconButton onClick={() => setProduct(product)}>
+                    <QrCodeScannerIcon color="primary" />
+                </IconButton>
+                <IconButton onClick={() => setProduct(product)}>
+                    <EditIcon color="primary" />
+                </IconButton>
+            </div>
         </Paper>
     )
 }
