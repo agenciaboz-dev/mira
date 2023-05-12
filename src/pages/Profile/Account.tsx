27572f04ae04aa1f187c58c759ef7bc8c86719f4
nandobfer <nandobfer@yahoo.com.br>
2023-05-12 11:27:34 -0300

    update: finished inputs

--- src/pages/Profile/Account.tsx
+++ src/pages/Profile/Account.tsx
@@ -1,5 +1,7 @@
 import { Form, Formik } from "formik"
 import React from "react"
+import MaskedInput from "react-text-mask"
+import { Button } from "../../components/Button"
 import { TextField } from "../../components/TextField"
 import { User } from "../../definitions/user"
 import { useColors } from "../../hooks/useColors"
@@ -9,16 +11,20 @@ interface AccountProps {
     user: User
 }
 
-interface FormValues {
-    name: string
-    email: string
-    phone: string
+interface FormValues extends User {
+    new_password: string
+    confirm_password: string
 }
 
 export const Account: React.FC<AccountProps> = ({ user }) => {
     const colors = useColors()
 
-    const initialValues: FormValues = user
+    const initialValues: FormValues = {
+        ...user,
+        password: "",
+        new_password: "",
+        confirm_password: "",
+    }
 
     const handleSubmit = (values: FormValues) => {
         console.log(values)
@@ -44,13 +50,50 @@ export const Account: React.FC<AccountProps> = ({ user }) => {
                             onChange={handleChange}
                             InputLabelProps={{ sx: styles.textfield }}
                         />
-                        <TextField
+                        <MaskedInput
+                            mask={["(", /\d/, /\d/, ")", " ", /\d/, /\d/, /\d/, /\d/, /\d/, "-", /\d/, /\d/, /\d/, /\d/]}
+                            guide={false}
                             name="phone"
-                            label="Telefone"
                             value={values.phone}
                             onChange={handleChange}
-                            InputLabelProps={{ sx: styles.textfield }}
+                            render={(ref, props) => (
+                                <TextField
+                                    inputRef={ref}
+                                    {...props}
+                                    label="Telefone"
+                                    InputLabelProps={{ sx: styles.textfield }}
+                                />
+                            )}
                         />
+
+                        <h3>Alteração de senha</h3>
+                        <p>(deixa em branco para não alterar)</p>
+                        <TextField
+                            name="password"
+                            label="Senha atual"
+                            value={values.password}
+                            onChange={handleChange}
+                            InputLabelProps={{ sx: styles.textfield }}
+                            type="password"
+                        />
+                        <TextField
+                            name="new_password"
+                            label="Nova senha"
+                            value={values.new_password}
+                            onChange={handleChange}
+                            InputLabelProps={{ sx: styles.textfield }}
+                            type="password"
+                        />
+                        <TextField
+                            name="confirm_password"
+                            label="Confirmar senha"
+                            value={values.confirm_password}
+                            onChange={handleChange}
+                            InputLabelProps={{ sx: styles.textfield }}
+                            type="password"
+                        />
+
+                        <Button type="submit">Salvar</Button>
                     </Form>
                 )}
             </Formik>
