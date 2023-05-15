5a31a042a00f8a632b5bb31adeb577dd840ac134
nandobfer <nandobfer@yahoo.com.br>
2023-05-15 17:12:22 -0300

    update: checkbox style props

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -6,16 +6,20 @@ interface CheckboxProps {
     value: boolean
     handleChange: (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => void
     label: string
+    labelStyle?: React.CSSProperties
+    checkboxStyle?: React.CSSProperties
 }
 
-export const Checkbox: React.FC<CheckboxProps> = ({ value, handleChange, label }) => {
+export const Checkbox: React.FC<CheckboxProps> = ({ value, handleChange, label, labelStyle, checkboxStyle }) => {
     const colors = useColors()
 
     return (
         <FormControlLabel
             sx={{ gap: "3vw", margin: "0", whiteSpace: "nowrap" }}
+            style={labelStyle}
             control={
                 <MuiCheckbox
+                    style={checkboxStyle}
                     onChange={handleChange}
                     sx={{
                         "&.Mui-checked": {
