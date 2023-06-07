e9248b4dd704eaf447d8cbded9c915e3f05335fa
tainarasn <tainarasn.sba@gmail.com>
2023-06-07 13:18:21 -0300

    feat: create component Service and styling freteModal

--- src/components/FreteModal/index.tsx
+++ src/components/FreteModal/index.tsx
@@ -1,6 +1,9 @@
 import { Box, Dialog, DialogTitle } from "@mui/material"
 import React from "react"
 import styles from "./styles"
+import { TextField } from "../TextField"
+import { Button } from "../Button"
+import { Service } from "./Service"
 
 interface FreteModalProps {
     open: boolean
@@ -13,16 +16,36 @@ export const FreteModal: React.FC<FreteModalProps> = ({ open, setOpen }) => {
     }
 
     return (
-        <Dialog open={open} onClose={handleClose} sx={styles.dialog}>
+        <Dialog open={open} onClose={handleClose} sx={styles.dialog} PaperProps={{ sx: styles.paper }}>
             <DialogTitle sx={styles.title}>Calcule o frete</DialogTitle>
             <Box sx={styles.body}>
-                <p>oi</p>
-                <p>oi</p>
-                <p>oi</p>
-                <p>oi</p>
-                <p>oi</p>
-                <p>oi</p>
-                <p>oi</p>
+                <TextField label="CEP" placeholder="" name="" sx={styles.input}></TextField>
+                <Box sx={styles.services}>
+                    {" "}
+                    <Service />
+                    <Service />
+                    <Service />
+                </Box>
+                <Box sx={styles.footer}>
+                    <div style={{ flexDirection: "column", alignItems: "flex-end", gap: "1.5vw" }}>
+                        <p>
+                            <span style={{ fontWeight: "600", color: "#555555", fontSize: "3.6vw" }}>
+                                Subtotal de produtos:
+                            </span>{" "}
+                            R$238,00
+                        </p>
+                        <p>
+                            <span style={{ fontWeight: "600", color: "#555555", fontSize: "3.6vw" }}>Frete:</span> R$15,00
+                        </p>
+                        <p>
+                            <span style={{ fontWeight: "600", color: "#555555", fontSize: "3.6vw" }}>Total do pedido:</span>{" "}
+                            R$253,00
+                        </p>
+                    </div>
+                    <Button type="submit" style={{ width: "78vw", fontSize: "4vw" }}>
+                        Continuar
+                    </Button>
+                </Box>
             </Box>
         </Dialog>
     )
--- src/components/FreteModal/styles.ts
+++ src/components/FreteModal/styles.ts
@@ -2,8 +2,13 @@ import { SxProps } from "@mui/material"
 
 interface Styles {
     dialog: SxProps
+    paper: SxProps
     body: SxProps
     title: SxProps
+    input: SxProps
+    footer: SxProps
+    radio: SxProps
+    services: SxProps
 }
 
 const styles: Styles = {
@@ -11,10 +16,54 @@ const styles: Styles = {
         flexDirection: "column",
     },
 
+    paper: {
+        flexDirection: "column",
+        width: "87vw",
+        height: "168vw",
+        borderRadius: "7vw",
+        position: "absolute",
+        bottom: "17vw",
+    },
+
     body: {
         flexDirection: "column",
+        padding: "0 3vw 5vw 3vw ",
+        height: "2vw",
     },
-    title: {},
+
+    title: {
+        fontWeight: "700",
+        fontSize: "4.3vw",
+        color: "#555555",
+    },
+
+    input: {
+        padding: "1vw",
+        position: "absolute",
+        right: "3.5vw",
+        width: "80vw",
+    },
+
+    footer: {
+        display: "flex",
+        flexDirection: "column",
+        position: "absolute",
+        top: "132vw",
+        gap: "4vw",
+        padding: "1vw",
+    },
+
+    radio: {
+        flexDirection: "column",
+        gap: "2vw",
+        position: "relative",
+        left: "1vw",
+        top: "25vw",
+    },
+    services: {
+        flexDirection: "column",
+        padding: "1vw",
+    },
 }
 
 export default styles
