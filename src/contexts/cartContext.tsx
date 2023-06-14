a71701a9c264e503c1117b2d515ce08768d179db
ispadilha <israel.padilha@gmail.com>
2023-06-14 12:30:27 -0300

    update: logout empties the cart on tablet

--- src/contexts/cartContext.tsx
+++ src/contexts/cartContext.tsx
@@ -1,7 +1,6 @@
-import { createContext, useEffect, useState } from "react"
+import { createContext, useState } from "react"
 import React from "react"
 import { Cart } from "../definitions/cart"
-import { useLocalStorage } from "../hooks/useLocalStorage"
 
 interface CartContextValue {
     value: Cart[]
@@ -17,13 +16,7 @@ const CartContext = createContext<CartContextValue>({} as CartContextValue)
 export default CartContext
 
 export const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
-    const storage = useLocalStorage()
-    const [value, setValue] = useState<Cart[]>(storage.get("mira.cart") || [])
-
-    useEffect(() => {
-        storage.set("mira.cart", value)
-        console.log({ cart: value })
-    }, [value])
+    const [value, setValue] = useState<Cart[]>([])
 
     return <CartContext.Provider value={{ value, setValue }}>{children}</CartContext.Provider>
 }
--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -2,16 +2,19 @@ import { useContext } from "react"
 import { useNavigate } from "react-router-dom"
 import UserContext from "../contexts/userContext"
 import { useLocalStorage } from "./useLocalStorage"
+import { useCart } from "./useCart"
 
 export const useUser = () => {
     const userContext = useContext(UserContext)
     const navigate = useNavigate()
     const storage = useLocalStorage()
+    const {setCart} = useCart()
 
     const logout = () => {
         userContext.setValue(null)
         storage.set("mira.user", null)
         navigate("/")
+        setCart([])
     }
 
     return { user: userContext.value, setUser: userContext.setValue, logout }
