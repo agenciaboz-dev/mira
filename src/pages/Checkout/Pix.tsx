a539ebf08e330a7db4c941bd36e692048eaea13a
ispadilha <israel.padilha@gmail.com>
2023-06-15 09:42:18 -0300

    update: pix-component items disposition

--- src/pages/Checkout/Pix.tsx
+++ src/pages/Checkout/Pix.tsx
@@ -60,9 +60,20 @@ export const Pix: React.FC<PixProps> = ({}) => {
 
     return (
         <div className="Pix-Component">
+            
             <h4>Como você gostaria de efetuar o pagamento do PIX?</h4>
             <p>Mostre o QR code na sua tela ou copie o código copia e cola para efetuar o pagamento.</p>
             <p>Esse código é válido por 1 hora.</p>
+
+            <div className="totals-container">
+                <h4>Total do pedido</h4>
+                <CurrencyText value={total} style={{ color: "black", fontSize: "10vw", fontWeight: "bold" }} />
+            </div>
+
+            <Button onClick={handleClick} style={{ padding: "2vw 12vw", gap: "10vw", justifyContent: "flex-start" }}>
+                <CopyIcon style={{ width: "7vw", height: "auto" }} />
+                {buttonText}
+            </Button>
 
             <div className="code-wrapper">
                 {qrCodeValue ? (
@@ -76,15 +87,6 @@ export const Pix: React.FC<PixProps> = ({}) => {
                 )}
             </div>
 
-            <div className="totals-container">
-                <h4>Total do pedido</h4>
-                <CurrencyText value={total} style={{ color: "black", fontSize: "10vw", fontWeight: "bold" }} />
-            </div>
-
-            <Button onClick={handleClick} style={{ padding: "2vw 12vw", gap: "10vw", justifyContent: "flex-start" }}>
-                <CopyIcon style={{ width: "7vw", height: "auto" }} />
-                {buttonText}
-            </Button>
         </div>
     )
 }
