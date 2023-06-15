9ed3cc136a203cef5779b414e68f35f3e07672de
ispadilha <israel.padilha@gmail.com>
2023-06-15 09:33:25 -0300

    Adjusted product story close button position

--- src/components/ProductStory/index.tsx
+++ src/components/ProductStory/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Product } from "../../definitions/product"
 import { Dialog, DialogContent, DialogContentText, DialogTitle, IconButton } from "@mui/material"
 import { styles } from "./styles"
-import CancelPresentationIcon from "@mui/icons-material/CancelPresentation"
+import CancelIcon from "@mui/icons-material/Cancel"
 import { useColors } from "../../hooks/useColors"
 
 interface ProductStoryProps {
@@ -20,12 +20,12 @@ export const ProductStory: React.FC<ProductStoryProps> = ({ product, open, setOp
     }
 
     return (
-        <Dialog open={open} onClose={handleClose} sx={styles.dialog}>
+        <Dialog open={open} onClose={handleClose} sx={styles.dialog} PaperProps={{ style: { borderRadius: "4vw" } }} >
             <DialogTitle sx={styles.title}>
-                <IconButton onClick={() => setOpen(false)} sx={{ position: "absolute", left: "3vw" }}>
-                    <CancelPresentationIcon color="error" sx={styles.close_icon} />
-                </IconButton>
                 {product?.name}
+                <IconButton onClick={() => setOpen(false)} sx={{ position: "absolute", top: 0, right: 0 }}>
+                    <CancelIcon color="error" sx={styles.close_icon} />
+                </IconButton>
             </DialogTitle>
 
             <DialogContent sx={styles.content_container}>
