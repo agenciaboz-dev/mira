a13f31e333c1a85ce7f273f559218afb2de8a75a
nandobfer <nandobfer@yahoo.com.br>
2023-06-19 09:47:39 -0300

    feat: card number visibility button

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,20 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import "./style.scss"
 import { Card as CardType } from "../../definitions/user"
 import VisibilityIcon from "@mui/icons-material/Visibility"
+import { IconButton } from "@mui/material"
 
 interface CardProps {
     card: CardType
 }
 
 export const Card: React.FC<CardProps> = ({ card }) => {
+    const [cardNumberView, setCardNumberView] = useState(false)
+
     return (
         <div className="Card-Component">
             <div className="cardtop-container">
                 <p>Número do cartão</p>
-                <VisibilityIcon />
+                <IconButton onClick={() => setCardNumberView(!cardNumberView)} sx={{ color: "white" }}>
+                    <VisibilityIcon />
+                </IconButton>
             </div>
-            <h4>{card.number || "0000 0000 0000 0000"}</h4>
+            <h4>
+                {cardNumberView ? card.number || "0000 0000 0000 0000" : `**** **** **** ${card.number.split(" ").pop()}`}
+            </h4>
 
             <div className="name-expiration">
                 <h3>{card.name || "Nome"}</h3>
