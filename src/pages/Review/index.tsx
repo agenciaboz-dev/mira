9643f7045db924e65d3a759792d057f058812456
ispadilha <israel.padilha@gmail.com>
2023-06-26 12:29:25 -0300

    Progressing reviews page

--- src/pages/Review/index.tsx
+++ src/pages/Review/index.tsx
@@ -5,29 +5,28 @@ import { useNavigate } from "react-router-dom"
 import { CircularProgress } from "@mui/material"
 import Rating from '@mui/material/Rating';
 
+import { useProducts } from "../../hooks/useProducts"
+import { Product } from "./Product"
+import { useCart } from "../../hooks/useCart"
+
+interface CartProps {}
+
 export const Review = () => {
     const navigate = useNavigate()
     const [loading, setLoading] = useState(false)
-    const [value, setValue] = React.useState<number | null>(2);
 
-    useEffect(() => {
-        console.log(value)
-    }, [value])
+    const { cart } = useCart()
 
     return <div className="Review-Page">
         <div className="title-container">
             <h2>Avaliação</h2>
         </div>
 
-        <div className="rating-container">
-            <Rating
-                name="simple-controlled"
-                value={value}
-                onChange={(event, newValue) => {
-                    setValue(newValue);
-                }}
-                />
-        </div>
+        <div className="product-list">
+                {cart.map((product) => (
+                    <Product key={product.id} product={product} />
+                ))}
+            </div>
 
         <div className="buttons-container">
             <Button onClick={() => navigate("/cart")} style={{ height: "10vw", width: "35vw", background: "linear-gradient(90deg, #9F9F9F 0%, #565656 91.94%)" }} >
