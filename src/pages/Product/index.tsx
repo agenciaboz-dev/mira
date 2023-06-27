c5cef8ce57c5b9cfb255a4a839e41395a206a872
tainarasn <tainarasn.sba@gmail.com>
2023-06-27 11:18:04 -0300

    fix: image product in carousel

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -56,7 +56,8 @@ export const Product: React.FC<ProductProps> = ({ product, style, innerRef, onCl
         }
     }
     //List to test product image carousel
-    const images = [{ id: 1 }, { id: 2 }, { id: 3 }]
+    const gallery = product.gallery?.split(",") || []
+    const images = [product.image, ...gallery]
 
     return (
         <div className="Product-Page" style={style} ref={innerRef}>
@@ -64,8 +65,8 @@ export const Product: React.FC<ProductProps> = ({ product, style, innerRef, onCl
                 <div className="carousel-container">
                     <Carousel showThumbs={false} autoPlay infiniteLoop={true} transitionTime={1200}>
                         {images.map((image) => (
-                            <div key={image.id} className="image" style={{ width: "100%" }}>
-                                <img src={product.image} alt="" />
+                            <div key={images.indexOf(image)} className="image" style={{ width: "100%" }}>
+                                <img src={image} alt={image} />
                             </div>
                         ))}
                     </Carousel>
