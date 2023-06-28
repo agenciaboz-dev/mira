85b966580548b45b78009ea7f6510c86b1135afc
ispadilha <israel.padilha@gmail.com>
2023-06-28 12:50:43 -0300

    update: order definition

--- src/definitions/cart.ts
+++ src/definitions/cart.ts
@@ -5,13 +5,19 @@ export interface Cart extends Product {
     quantity: number
 }
 
+interface orderProduct {
+    quantity: number
+    product: Product
+}
+
 export interface Order {
     id?: number
-    products?: Cart[]
+    products: orderProduct[]
     delivery?: boolean
     status?: number
     user?: User
     address?: Address
     total?: number
+    review: boolean
     quotation?: any
 }
