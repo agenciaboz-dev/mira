7e756d0c392e13656399968e4b05ae507b50eac0
ispadilha <israel.padilha@gmail.com>
2023-06-28 10:32:52 -0300

    Adjusted the similar items tutorial text balloon

--- src/components/SimilarItemsTutorialMask/index.tsx
+++ src/components/SimilarItemsTutorialMask/index.tsx
@@ -1,6 +1,6 @@
 import "./style.scss"
 import { ReactComponent as RectangularTutorialFocus } from "../../images/rectangular_tutorial_focus.svg"
-import { ReactComponent as Balloon } from "../../images/balloon.svg"
+import { ReactComponent as Balloon2 } from "../../images/balloon2.svg"
 import { ReactComponent as AvatarBG } from "../../images/avatar_bg.svg"
 import { useNavigate } from "react-router-dom"
 import { useLocalStorage } from "../../hooks/useLocalStorage"
@@ -25,7 +25,7 @@ export const SimilarItemsTutorialMask = () => {
         <div className="similar_items_tutorial-mask" onClick={completeSimilarItemsTutorial} >
             <RectangularTutorialFocus style={focus_style} className="focus"/>
             <div className="balloon-container">
-                <Balloon className="balloon" />
+                <Balloon2 className="balloon" />
                 <div className="balloon-text">
                     <p>
                         Caso queira ver produtos similares, basta clicar no botão indicado!
