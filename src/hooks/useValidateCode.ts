f3a600b98689d410ecbc58361c7b64d5ca260a59
nandobfer <nandobfer@yahoo.com.br>
2023-07-04 14:35:48 -0300

    fix: wrong base url

--- src/hooks/useValidateCode.ts
+++ src/hooks/useValidateCode.ts
@@ -1,10 +1,9 @@
 export const useValidadeCode = () => {
     const validate = (text: string) => {
-        const base = "https://play.google.com/store/apps/details?id=com.mira.guide&?pid=1000"
-        const splitted = text.split("&?pid=")
+        const splitted = text.split("?id=")
 
         if (splitted.length == 2) {
-            if (splitted[0] == "https://play.google.com/store/apps/details?id=com.mira.guide" && !!Number(splitted[1])) {
+            if (splitted[0] == "https://app.mirasuprimentos.com.br/download" && !!Number(splitted[1])) {
                 return Number(splitted[1])
             } else {
                 return
