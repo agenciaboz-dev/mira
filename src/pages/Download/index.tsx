cbd5c4c8515622e44c92c61e438b05f9b43b7a9f
tainarasn <tainarasn.sba@gmail.com>
2023-07-05 13:05:56 -0300

    fix: icons apple and android in page download

--- src/pages/Download/index.tsx
+++ src/pages/Download/index.tsx
@@ -1,9 +1,10 @@
 import { Box, SxProps } from "@mui/material"
 import React from "react"
 import { ReactComponent as LogoIcon } from "../../images/logo.svg"
+import { ReactComponent as AppleIcon } from "../../images/apple-download.svg"
+import { ReactComponent as GoogleIcon } from "../../images/google-download.svg"
 import { useColors } from "../../hooks/useColors"
-import AdbIcon from "@mui/icons-material/Adb"
-import AppleIcon from "@mui/icons-material/Apple"
+
 import { Button } from "../../components/Button"
 
 interface DownloadProps {}
@@ -13,11 +14,11 @@ export const Download: React.FC<DownloadProps> = ({}) => {
 
     const platform_style: SxProps = {
         flexDirection: "column",
-        gap: "5vw",
         alignItems: "center",
+        justifyContent: "center",
         fontSize: "6vw",
         color: "white",
-        width: "35vw",
+        gap: "5vw",
     }
 
     const platform_icon: SxProps = {
@@ -55,18 +56,28 @@ export const Download: React.FC<DownloadProps> = ({}) => {
 
             <Box sx={{ justifyContent: "space-between", width: "100%" }}>
                 <Box sx={platform_style}>
-                    <p>Android</p>
-                    <AdbIcon sx={platform_icon} />
-                    <Button fullWidth href="https://play.google.com/store/apps/details?id=com.mira.guide">
-                        Baixar
-                    </Button>
-                </Box>
-                <Box sx={platform_style}>
-                    <p>iOs</p>
-                    <AppleIcon sx={platform_icon} />
-                    <Button fullWidth href="https://play.google.com/store/apps/details?id=com.mira.guide">
-                        Baixar
-                    </Button>
+                    <a href="https://play.google.com/store/apps/details?id=com.mira.guide">
+                        <GoogleIcon
+                            style={{
+                                position: "relative",
+                                left: "4.5vw",
+                                width: "70vw",
+                                height: "auto",
+                                backgroundColor: "white",
+                            }}
+                        />
+                    </a>
+                    <a href="https://play.google.com/store/apps/details?id=com.mira.guide">
+                        <AppleIcon
+                            style={{
+                                position: "relative",
+                                left: "4.5vw",
+                                width: "70vw",
+                                height: "auto",
+                                backgroundColor: "white",
+                            }}
+                        />
+                    </a>
                 </Box>
             </Box>
         </Box>
