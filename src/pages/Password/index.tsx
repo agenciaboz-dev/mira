390574c57c37677b2174a139c3c24216592fa386
nandobfer <nandobfer@yahoo.com.br>
2023-08-03 17:04:52 -0300

    feat: added reset password

--- src/pages/Password/index.tsx
+++ src/pages/Password/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Box } from "@mui/material"
 import { Route, Routes } from "react-router-dom"
 import { Forgot } from "./Forgot"
+import { Reset } from "./Reset"
 
 interface PasswordProps {}
 
@@ -9,6 +10,7 @@ export const Password: React.FC<PasswordProps> = ({}) => {
     return (
         <Routes>
             <Route path="forgot" element={<Forgot />} />
+            <Route path="reset/:hash" element={<Reset />} />
         </Routes>
     )
 }
