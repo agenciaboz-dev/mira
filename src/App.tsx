608110fdc50c5a661c36d424252e2e9247158e2e
nandobfer <nandobfer@yahoo.com.br>
2023-08-03 15:47:32 -0300

    feat: added forgot password page

--- src/App.tsx
+++ src/App.tsx
@@ -21,84 +21,9 @@ import { useIdleTimer } from "react-idle-timer"
 import { TimeoutPrompt } from "./components/TimeoutPrompt"
 import { useReset } from "./hooks/useReset"
 import { ProdutList } from "./pages/ProductList"
-
-const timeout = 240_000
-const promptBeforeIdle = 120_000
-
-const Container = () => {
-    const location = useLocation()
-    const reset = useReset()
-
-    const [state, setState] = useState<string>("Active")
-    const [remaining, setRemaining] = useState<number>(timeout)
-    const [open, setOpen] = useState<boolean>(false)
-
-    const onIdle = () => {
-        if (location.pathname != "/" && location.pathname != "/login") {
-            setState("Idle")
-            setOpen(false)
-            reset()
-        }
-    }
-
-    const onActive = () => {
-        setState("Active")
-        setOpen(false)
-    }
-
-    const onPrompt = () => {
-        if (location.pathname != "/" && location.pathname != "/login") {
-            setState("Prompted")
-            setOpen(true)
-        }
-    }
-
-    const { getRemainingTime, activate } = useIdleTimer({
-        onIdle,
-        onActive,
-        onPrompt,
-        timeout,
-        promptBeforeIdle,
-        throttle: 500,
-    })
-
-    const handleStillHere = () => {
-        activate()
-    }
-
-    useEffect(() => {
-        console.log(state)
-    }, [state])
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setRemaining(Math.ceil(getRemainingTime() / 1000))
-        }, 500)
-
-        return () => {
-            clearInterval(interval)
-        }
-    })
-
-    const timeTillPrompt = Math.max(remaining - promptBeforeIdle / 1000, 0)
-    const seconds = timeTillPrompt > 1 ? "seconds" : "second"
-
-    return (
-        <>
-            <TimeoutPrompt open={open} setActive={() => handleStillHere()} />
-            <Routes>
-            <Route index element={<Login />} />
-                                            <Route path="/*" element={<Login />} />
-                                            <Route path="/login/*" element={<Login />} />
-                                            <Route path="/cart" element={<Cart />} />
-                                            <Route path="/scan" element={<Camera />} />
-                                            <Route path="/profile/*" element={<Profile />} />
-                <Route path="/checkout/*" element={<Checkout />} />
-                <Route path="/products" element={<ProdutList />} />
-            </Routes>
-        </>
-    )
-}
+import { Download } from "./pages/Download"
+import { Password } from "./pages/Password"
+import { Review } from "./pages/Checkout/Review"
 
 function App() {
     const muiTheme = useMuiTheme()
@@ -113,14 +38,27 @@ function App() {
                                 <AddressProvider>
                                     <BrowserRouter>
                                         <Snackbar />
-                                        <Container />
-                                    </BrowserRouter>
-                                </AddressProvider>
-                            </CartProvider>
-                        </ProductsProvider>
-                    </UserProvider>
-                </OrdersProvider>
-            </SnackbarProvider>
+                                        <Routes>
+                                            <Route index element={<Login />} />
+                                            <Route path="/*" element={<Login />} />
+                                            <Route path="/login/*" element={<Login />} />
+                                            <Route path="/download" element={<Download />} />
+                                            <Route path="/cart" element={<Cart />} />
+                                            <Route path="/scan" element={<Camera />} />
+                                            <Route path="/profile/*" element={<Profile />} />
+                                            <Route path="/checkout/*" element={<Checkout />} />
+                                            <Route path="/review" element={<Review />} />
+                                            <Route path="/products" element={<ProdutList />} />
+                                            <Route path="/password/*" element={<Password />} />
+                                        </Routes>
+                                        </BrowserRouter>
+                                    </AddressProvider>
+                                </CartProvider>
+                            </ProductsProvider>
+                        </UserProvider>
+                    </OrdersProvider>
+                </SnackbarProvider>
+            
         </ThemeProvider>
     )
 }
