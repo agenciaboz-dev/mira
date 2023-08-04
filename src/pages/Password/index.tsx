e922d8de940932219419a74376f8ae89693f8e64
tainarasn <tainarasn.sba@gmail.com>
2023-08-04 08:56:12 -0300

    Revert "feat: added reset password"
    This reverts commit 390c62034b3ff7b8674c65336aaf7d9e4adfaa5d.

--- src/pages/Password/index.tsx
+++ src/pages/Password/index.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import { Box } from "@mui/material"
 import { Route, Routes } from "react-router-dom"
 import { Forgot } from "./Forgot"
-import { Reset } from "./Reset"
 
 interface PasswordProps {}
 
@@ -10,7 +9,6 @@ export const Password: React.FC<PasswordProps> = ({}) => {
     return (
         <Routes>
             <Route path="forgot" element={<Forgot />} />
-            <Route path="reset/:hash" element={<Reset />} />
         </Routes>
     )
 }
