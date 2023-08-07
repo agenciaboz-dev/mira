8462b2b31d44737f3b77272f2d209df765e3b117
tainarasn <tainarasn.sba@gmail.com>
2023-08-07 08:05:26 -0300

    fix: revert header buttons

--- src/pages/Password/index.tsx
+++ src/pages/Password/index.tsx
@@ -1,14 +1,14 @@
 import React from "react"
 import { Box } from "@mui/material"
 import { Route, Routes } from "react-router-dom"
-import { Forgot } from "./Forgot"
+//import { Forgot } from "./Forgot"
 
 interface PasswordProps {}
 
 export const Password: React.FC<PasswordProps> = ({}) => {
     return (
         <Routes>
-            <Route path="forgot" element={<Forgot />} />
+            {/* <Route path="forgot" element={<Forgot />} /> */}
         </Routes>
     )
 }
